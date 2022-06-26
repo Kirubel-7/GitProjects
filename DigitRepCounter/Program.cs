ae6cea77ff4c923649d160f41a1b751607ae45ff
Kirubel-7 <kismaname1@gmail.com>
2022-06-26 22:02:50 +0300

    Proj1

---
+++ DigitRepCounter/Program.cs
@@ -0,0 +1,22 @@
+﻿/////////////DigitRepCounter//////////////
+Console.Write("Enter an Integer:");
+int i=Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the digit:");
+int digit=Convert.ToInt32(Console.ReadLine());
+void repcounter(int i,int digit)//Function To check Number of rep of digit in an Integer
+{
+    int k=i,count = 0, rem=0,rep=0;
+    if (k < 0 || digit < 0)
+        Console.WriteLine("-1");
+    else {
+        while (k != 0) {
+            rem = k % 10;
+            k=k/10;
+            if (rem == digit) {
+                count++;
+            }
+        }
+    }
+    Console.WriteLine("The Number of times " + digit + " is Found in " + i + " is: " + count);
+}
+repcounter(i,digit);
---
+++ EvenOdd_Chk/Program.cs
@@ -0,0 +1,17 @@
+﻿////////////Even Or Odd Checker///////////////
+m : Console.Write("Enter an Integer:");
+int x=Convert.ToInt32(Console.ReadLine());
+void isEvenOdd(int m) {
+    int y = x;
+    if (y < 0)
+        Console.WriteLine("Not a Postive Integer!");
+    else if (y == 0)
+        Console.WriteLine("Neither Odd nor Even,");
+    else {
+        if (y % 2 == 0)
+            Console.WriteLine(y + " is Even.");
+        else
+            Console.WriteLine(y + " id Odd.");
+    }
+}
+isEvenOdd(x);
